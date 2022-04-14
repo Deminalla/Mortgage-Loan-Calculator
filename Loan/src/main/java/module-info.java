b9ef9a793b7abe575cb7cb4c5b9b5f378ef04681
Deniza <65849358+Deminalla@users.noreply.github.com>
2022-04-14 18:54:02 +0300

    Delete Loan directory

--- Loan/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module Loan {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens Controllers to javafx.fxml;
-    exports Controllers;
-    opens Helper to javafx.fxml;
-    exports Helper;
-}
